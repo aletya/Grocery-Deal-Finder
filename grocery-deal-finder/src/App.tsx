41e0d76dba3ac675ecc90c97e30fbecf5f88cb04
Sunny Lee <sunnyjl2@illinois.edu>
2024-02-29 04:41:46 -0600

    basic homepage created

--- grocery-deal-finder/src/App.tsx
+++ grocery-deal-finder/src/App.tsx
@@ -1,13 +1,10 @@
 import Logo from "@/assets/logo.png";
-import HelloWorld from "@/components/HelloWorld/HelloWorld";
+import Home from "@/components/Home/Home";
 
 import styles from "./App.module.css";
 
 export default function App() {
   return (
-    <main className={styles.main}>
-      <img className={styles.logo} alt="React logo" width="400px" src={Logo} />
-      <HelloWorld msg="Hello React + TypeScript + Vite" />
-    </main>
+    <Home />
   );
 }
