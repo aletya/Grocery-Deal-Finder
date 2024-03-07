e121032ecf72ac84fd5d9944f819405b0a03633a
Alex Yang <32620988+DatProJack@users.noreply.github.com>
2024-03-07 14:04:49 -0600

    week 3

--- countymart-scraper.py
+++ countymart-scraper.py
@@ -46,30 +46,48 @@ driver.get(new_url)
 # Wait for the modified URL page to load
 wait.until(EC.visibility_of_element_located((By.TAG_NAME, "body")))
 
-# Find the <div> element with the specified class
-div_elements = driver.find_elements(By.CSS_SELECTOR, "div.circular-item-body.card-body")
-
+#searches for the total page count
+span_elements = driver.find_elements(By.CLASS_NAME, "d-none")
+page_count = 0
+for span_element in span_elements:
+    text = span_element.text
+    if "Page 1 of " in text:
+        page_count = int(text[10:])
 
 title = []
 description = []
 price = []
-if div_elements:
-    for div_element in div_elements:
-        # If the element is found, find its child elements
-        h3_element = div_element.find_element(By.CLASS_NAME, "circular-item-title")
-        description_element = div_element.find_element(By.CLASS_NAME, "circular-item-description")
-        price_element = ""
-        try:
-            price_element = div_element.find_element(By.CLASS_NAME, "price-big-dollars")
-        except:
-            price_element = div_element.find_element(By.CLASS_NAME, "price-big-cents")
-        # Print the text contents of the child elements
-        #formatting the information into arrays
-        title.append(h3_element.text)
-        description.append(description_element.text)
-        price.append(price_element.text)
+for i in range(1, page_count):
+    url_parts = new_url.split("/")
+    index_page = url_parts.index("Page")
+    url_parts[index_page + 1] = str(i)
+    new_url = "/".join(url_parts)
+    driver.get(new_url)
+    wait.until(EC.visibility_of_element_located((By.TAG_NAME, "body")))
+    # Find the <div> element with the specified class
+    div_elements = driver.find_elements(By.CSS_SELECTOR, "div.circular-item-body.card-body")
+    if div_elements:
+        for div_element in div_elements:
+            # If the element is found, find its child elements
+            h3_element = div_element.find_element(By.CLASS_NAME, "circular-item-title")
+            description_element = div_element.find_element(By.CLASS_NAME, "circular-item-description")
+            price_element = ""
+            try:
+                price_element = div_element.find_element(By.CLASS_NAME, "price-big-dollars")
+            except:
+                try:
+                    price_element = div_element.find_element(By.CLASS_NAME, "price-big-cents")
+                except:
+                    continue
+            # Print the text contents of the child elements
+            #formatting the information into arrays
+            title.append(h3_element.text)
+            description.append(description_element.text)
+            price.append(price_element.text)
 
 for i in range(0, len(title)):
+    price[i] = price[i].replace(" ","", 2)
+    price[i] = price[i].replace(" "," per ", 1)
     print("Title: " + title[i])
     print("Description: " + description[i])
     print("Price: " + price[i])
