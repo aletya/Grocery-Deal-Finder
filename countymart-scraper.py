87e258070667d749d9fb1990db31664c05f853d7
Alex Yang <32620988+DatProJack@users.noreply.github.com>
2024-03-20 14:31:03 -0500

    added finding closest store by zip code

--- countymart-scraper.py
+++ countymart-scraper.py
@@ -7,9 +7,23 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.keys import Keys
+import time
+from geopy.geocoders import GoogleV3
+from geopy.distance import geodesic
 
+#guys don't spam the api and drain my wallet pls and ty
+geolocator = GoogleV3(api_key='AIzaSyBn2kFTM8XxS0bf44VdhFro625CY0017xY')
 
+def get_location(address):
+    location = geolocator.geocode(address)
+    return location.latitude, location.longitude
 
+def calculate_distance(address1, address2):
+    location1 = get_location(address1)
+    location2 = get_location(address2)
+    return geodesic(location1, location2).kilometers
+
+zip_code = input("Please enter your zip code: ")
 
 # Initializes a selenium webdriver for Chrome
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -18,15 +32,49 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://www2.mycountymarket.com/circulars/select_a_store/")
 driver.maximize_window()
 
+
+# Searches for each tab to toggle visible store locations
+tabs = driver.find_elements(By.CLASS_NAME, "stores-states-link.nav-link")
+
+full_addresses = []
+for tab in tabs:
+    tab.click()
+    # Searches for all store addresses
+    time.sleep(1)
+    temp_addresses = driver.find_elements(By.CLASS_NAME, "store-list-item-address.col")
+    temp_cities = driver.find_elements(By.CLASS_NAME, "store-list-item-city.col-auto")
+    for i in range(len(temp_addresses)):
+        if temp_addresses[i].text != "":
+            full_addresses.append(temp_addresses[i].text + ', ' + temp_cities[i].text)
+
+min_dist = 100000000
+closest_store = ""
+for full_address in full_addresses:
+    cur_dist = calculate_distance(full_address, zip_code)
+    #print(full_address + ' to ' + zip_code + ': ' + str(cur_dist) + 'km')
+    if(cur_dist < min_dist):
+        min_dist = cur_dist
+        closest_store = full_address
+
 # Searches for all elements with a link
 links = driver.find_elements("xpath","//a[@href]")
 
-# NOTE: I'm hard coding for a certain location right now, but I'll add functionality to choose stores later
+# Extract street address of closest store
+closest_store = closest_store.split(',')[0]
+print("The closest store to you is " + closest_store + ", " + str(min_dist) + " km away")
+
+# Searches for the closest store's link in each tab
 my_link = ""
-for link in links:
-    if "331 Stoughton Street" in link.text:
-        my_link = link
-print(my_link.get_attribute("href"))
+for tab in tabs:
+    tab.click()
+    found = False
+    for link in links:
+        if closest_store in link.text:
+            my_link = link
+            found = True
+            break
+    if found:
+        break
 
 # Follows link
 my_link.click()
