217fd9278b2c8b7e953a64d448bcede4f83953d3
Alex Yang <32620988+DatProJack@users.noreply.github.com>
2024-03-27 14:43:16 -0500

    moved backend into its own dir

---
+++ backend/backend.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from countymart_scraper import get_deals
+
+app = Flask(__name__)
+
+@app.route('/')
+def test():
+    return "API is working!"
+
+@app.route('/county-market', methods=['GET'])
+def county_market_deals():
+    user_zip_code = request.args.get('zip_code')
+    if user_zip_code:
+        user_zip_code = int(user_zip_code)
+        return jsonify({'deals': get_deals(user_zip_code)}), 200
+    else:
+        return jsonify({'error': 'Status code not provided'}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
