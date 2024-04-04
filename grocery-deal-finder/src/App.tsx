1d00e3b3dea73f2c5e43e3f08e8187125ac2b6f5
Sunny Lee <sunnyjl2@illinois.edu>
2024-04-04 14:14:03 -0500

    added ryans homepage button code

--- grocery-deal-finder/src/App.tsx
+++ grocery-deal-finder/src/App.tsx
@@ -4,8 +4,15 @@ import Results from "@/components/Results/Results";
 
 import styles from "./App.module.css";
 
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
 export default function App() {
   return (
-    <Results />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Results />} />
+      </Routes>
+    </Router>
   );
 }
